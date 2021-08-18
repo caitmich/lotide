63d5b8caed2d2f007b8c3066de180e9fdac3749d
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-18 15:45:21 -0400

    incremental coding using objects

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+//assertEqual function:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+//iterate through array
+  for(const item of allItems){
+//count items included in object itemsToCount
+if(itemsToCount[item] === true){
+    if(results[item]){
+      results[item] += 1;
+    } else{
+      results[item] =1;
+    }
+  }
+}
+//   //return an object
+return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
