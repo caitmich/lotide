edf5b316de4f1cc9a6e9c204d520a73ebd8e0318
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-23 15:39:36 -0400

    middleTest using chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
+const assert = require('chai').assert;
+
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-//Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+describe("#tail", () => {
 
-tail(['yes']);
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+});
