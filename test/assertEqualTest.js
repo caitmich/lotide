39ca5eb0396a743c8ef397a29f567304cce8e5f0
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-23 14:04:37 -0400

    testing tail

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("myName", "myname");
+assertEqual("myname", "myname");
+assertEqual(3, 3);
+assertEqual(3, 333);
