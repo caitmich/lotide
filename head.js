5ea33fa68050a5ee46f1a6a25e3a7035fc023660
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-23 20:14:19 -0400

    head.js

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = require('./assertEqual');
 
 
 
 const head = function(arr){
   return arr[0];
 };
-// console.log(head([5, 6, 7]));
-// console.log(head(['hello', 6, 7]));
-
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 module.exports = head;
